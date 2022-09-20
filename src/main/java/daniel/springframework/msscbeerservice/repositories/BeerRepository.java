42a0d23f5a337be8a3c3af06cd57d6542dd98232
Daniel Melo <daniel.melo42@outlook.com>
2022-09-19 18:34:10 -0600

    beer repository added. closes #6

---
+++ src/main/java/daniel/springframework/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,10 @@
+package daniel.springframework.msscbeerservice.repositories;
+
+import daniel.springframework.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+
+}
